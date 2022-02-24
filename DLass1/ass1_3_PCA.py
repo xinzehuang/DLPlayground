ee7a7d54cd3a6e7a31f76305adf02af5736b1f11
xinzehuang <44016281+xinzehuang@users.noreply.github.com>
2022-02-25 00:12:29 +0800

    Update ass1_3_PCA.py

--- DLass1/ass1_3_PCA.py
+++ DLass1/ass1_3_PCA.py
@@ -28,7 +28,7 @@ data = scio.loadmat(dataFile)
 # print(data['fea'].shape)
 # print(data['gnd'].shape)
 
-def get_acc(m=10, k=2, p=1):
+def get_acc(m=20, k=2, p=1):
     def get_dataset_random(data, m=m):
         features = data['fea']
         labels = data['gnd']
@@ -60,13 +60,13 @@ def get_acc(m=10, k=2, p=1):
         for label in remove_duplicates_labels_set:
             temp_list = label_featureindex_dict[str(label)].copy()
             random.shuffle(temp_list)
-            for i in temp_list[:-20]:
+            for i in temp_list[:m]:
                 train_feature.append(i)
-            for i in temp_list[-20:]:
+            for i in temp_list[m:]:
                 test_feature.append(i)
-            for i in range(len(temp_list)-20):
+            for i in range(m):
                 train_label.append(label)
-            for i in range(20):
+            for i in range(len(temp_list)-m):
                 test_label.append(label)
 
         return train_feature, test_feature, train_label, test_label
@@ -74,22 +74,35 @@ def get_acc(m=10, k=2, p=1):
     # generate train val test
     train_feature, test_feature, train_label, test_label = get_dataset_random(data=data, m=m)
 
-    from sklearn.model_selection import GridSearchCV
 
-    parameters = {'n_neighbors': [2, 3, 5, 10],
-                  'p': [1, 2, 3]}
-    knn = KNeighborsClassifier(n_neighbors=k, p=p)
-    clf = GridSearchCV(knn, parameters, scoring="accuracy")
-    clf.fit(train_feature, train_label)
-    print(clf.best_params_)
-    print(clf.best_score_)
+    ############################## PCA #################################################################
 
-    return 1
+    n_components = 150
+
+    pca = PCA(n_components=n_components, svd_solver='randomized',
+              whiten=True).fit(train_feature)
+
+    eigenfaces = pca.components_.reshape((n_components, 32, 32))
+
+    train_feature_pca = pca.transform(train_feature)
+    test_feature_pca = pca.transform(test_feature)
+
+
+    # knn
+
+    knn = KNeighborsClassifier(n_neighbors=1, p=2)
+    knn.fit(train_feature_pca, train_label)
+    y_predict = knn.predict(test_feature_pca)
+
+    acc = accuracy_score(test_label, y_predict)
+
+
+    return acc
 
 
 if __name__ == '__main__':
 
-    get_acc()
+    print(get_acc())
 
 
 
